72d30f44720288f2b9344edd436a2e6e22a214b5
carbonsushi <94692480+carbonsushi@users.noreply.github.com>
2022-01-29 02:58:46 +0900

    Fix bug

--- app/src/main/java/io/github/carbonsushi/mishutter/Hooker.kt
+++ app/src/main/java/io/github/carbonsushi/mishutter/Hooker.kt
@@ -10,7 +10,13 @@ class Hooker : IXposedHookLoadPackage {
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         if (lpparam.packageName == "com.android.camera") {
             EzXHelperInit.initHandleLoadPackage(lpparam)
-            getMethod("com.xiaomi.camera.util.SystemProperties", "get").hookBefore { param ->
+            getMethod(
+                "com.xiaomi.camera.util.SystemProperties",
+                "get",
+                String::class.java,
+                true,
+                String::class.java
+            ).hookBefore { param ->
                 when ((param.args[0] as String)) {
                     "ro.miui.region" -> param.result = "US"
                     "ro.miui.customized.region" -> param.result = "us"
