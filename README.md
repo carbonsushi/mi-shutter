b3da6ab344593c24853d2c174585ca21121f0133
carbonsushi <94692480+carbonsushi@users.noreply.github.com>
2022-01-29 01:55:29 +0900

    Create app

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mi shutter
+
+Xposed module to force the display of MIUI camera shutter sound settings.
+
+The process is similar to setting the region setting to the US, so there may be some inconveniences.
---
+++ app/src/main/java/io/github/carbonsushi/mishutter/Hooker.kt
@@ -0,0 +1,21 @@
+package io.github.carbonsushi.mishutter
+
+import com.github.kyuubiran.ezxhelper.init.EzXHelperInit
+import com.github.kyuubiran.ezxhelper.utils.getMethod
+import com.github.kyuubiran.ezxhelper.utils.hookBefore
+import de.robv.android.xposed.IXposedHookLoadPackage
+import de.robv.android.xposed.callbacks.XC_LoadPackage
+
+class Hooker : IXposedHookLoadPackage {
+    override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
+        if (lpparam.packageName == "com.android.camera") {
+            EzXHelperInit.initHandleLoadPackage(lpparam)
+            getMethod("com.xiaomi.camera.util.SystemProperties", "get").hookBefore { param ->
+                when ((param.args[0] as String)) {
+                    "ro.miui.region" -> param.result = "US"
+                    "ro.miui.customized.region" -> param.result = "us"
+                }
+            }
+        }
+    }
+}
